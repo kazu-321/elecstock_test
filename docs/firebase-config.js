4316924795f8e72f19d559e40aef9c6538ed87f5
kazu-321 <kzs321kzs@gmail.com>
2026-07-21 09:44:07 +0900

    chore: configure Firebase web app

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebaseコンソールの「プロジェクトの設定」>「マイアプリ」>「ウェブアプリ」からコピーします。
 // この設定値はブラウザに公開される前提のものです。データ保護はFirestore Security Rulesで行います。
 export const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.firebasestorage.app',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: 'AIzaSyAzbdou5M1jNkOV5EsWn_jv1zjPtBI-Rt8',
+  authDomain: 'elecstock-test.firebaseapp.com',
+  projectId: 'elecstock-test',
+  storageBucket: 'elecstock-test.firebasestorage.app',
+  messagingSenderId: '604072399702',
+  appId: '1:604072399702:web:40608590eac14426783cf7'
 };
 
 export const isFirebaseConfigured = !Object.values(firebaseConfig).some((value) => String(value).startsWith('YOUR_'));
